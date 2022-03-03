c010d87fb1565cdaf3335f353175e6ae4c5f9cff
priyanshi <priyanshi@DESKTOP-ORCB4LT>
2022-03-04 02:50:58 +0530

    first commit

---
+++ java/javacore/src/java11/a1.java
@@ -0,0 +1,14 @@
+package java11;
+
+@FunctionalInterface
+interface Func<T1,T2,T3,R> {
+     R apply(T1 t1, T2 t2, T3 t3);
+}
+public class a1
+{
+    public static void main (String[] args)
+    {
+        Func <Integer,Integer,Integer,Integer>  i = ( var p,var j,var r) -> (p*j*r);
+        System.out.println(i.apply(10,20,50));
+    }
+}
