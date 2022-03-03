180812f6426ce1474a7a579afcec0349163fdac1
priyanshi <priyanshi@DESKTOP-ORCB4LT>
2022-03-04 03:02:46 +0530

    first commit

---
+++ junit/junit/src/main/Lifecycle.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Lifecycle {
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static int sub(int a, int b){
+        return a-b;
+    }
+
+}
